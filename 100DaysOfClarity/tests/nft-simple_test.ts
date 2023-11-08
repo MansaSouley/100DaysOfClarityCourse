4ba43aaaa0f58b13ac853fec2de9d9cfcaaf3907
MansaSouley <souleythemane@gmail.com>
2023-11-07 21:49:45 -0600

    Day 79 - Simple FT IV

--- 100DaysOfClarity/tests/nft-simple_test.ts
+++ 100DaysOfClarity/tests/nft-simple_test.ts
@@ -2,59 +2,49 @@
 import { Clarinet, Tx, Chain, Account, Contract, types } from 'https://deno.land/x/clarinet@v0.31.0/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
+// Two tests
+// 1. Can mint one FT
+// 2. Cannot mint another
+
 Clarinet.test({
-    name: "Ensure that the right amount of STX(nft price) is transferred by expectingEvent",
-    async fn(chain: Chain, accounts: Map<string, Account>, conctracts: Map<string, Contract>){
+    name: "Ensure that exactly 1 CT is transferred on mint",
+    async fn(chain: Chain, accounts: Map<string, Account>, Contracts: Map<string, Contract>){
         let deployer = accounts.get("deployer")!;
-        let wallet_1 = accounts.get("wallet_1")!;
 
         let block = chain.mineBlock([
-            Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
-        ]);
+            Tx.contractCall("simple-ft", "claim-ct", [], deployer.address)
+        ])
 
-        block.receipts[0].result.expectOk().expectBool(true);
-        console.log(block.receipts[0].events);
-        block.receipts[0].events.expectSTXTransferEvent(
-            10000000,
-            wallet_1.address,
-            `${deployer.address}.nft-simple`
+        console.log(block.receipts[0].events)
+
+        block.receipts[0].events.expectFungibleTokenMintEvent(
+            1,
+            deployer.address,
+            "clarity-token"
         )
     }
 })
 
 Clarinet.test({
-    name: "Ensure that the right amount of STX(nft price) is transferred by checking balances",
-    async fn(chain: Chain, accounts: Map<string, Account>, conctracts: Map<string, Contract>){
+    name: "Ensure that same principal cannot claim twice",
+    async fn(chain: Chain, accounts: Map<string, Account>, Contracts: Map<string, Contract>){
         let deployer = accounts.get("deployer")!;
-        let wallet_1 = accounts.get("wallet_1")!;
 
         let block = chain.mineBlock([
-            Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
-        ]);
+            Tx.contractCall("simple-ft", "claim-ct", [], deployer.address)
+        ])
 
-        block.receipts[0].result.expectOk().expectBool(true);
-        console.log(block.receipts[0].events);
-        assertEquals(chain.getAssetsMaps().assets['STX'][wallet_1.address], 99999990000000);
-    }
-})
+        console.log(block.receipts[0].events)
 
-Clarinet.test({
-    name: "Ensure that the right NFT is minted to the right address",
-    async fn(chain: Chain, accounts: Map<string, Account>, conctracts: Map<string, Contract>){
-        let deployer = accounts.get("deployer")!;
-        let wallet_1 = accounts.get("wallet_1")!;
+        chain.mineEmptyBlock(1)
 
-        let block = chain.mineBlock([
-            Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
-        ]);
+        let block2 = chain.mineBlock([
+            Tx.contractCall("simple-ft", "claim-ct", [], deployer.address)
+        ])
 
-        block.receipts[0].result.expectOk().expectBool(true);
-        console.log(block.receipts[0].events);
-        block.receipts[0].events.expectNonFungibleTokenMintEvent(
-            types.uint(1),
-            wallet_1.address,
-            `${deployer.address}.nft-simple`,
-            "simple-nft"
-        )
+        console.log(block2.receipts[0].events)
+        
+        block2.receipts[0].result.expectErr()
+       
     }
 })
